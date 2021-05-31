27f89f987f55cd94f7ec927b78dd9db942ffb851
André <ruland@outlook.de>
2021-05-31 12:00:13 +0200

    Simple Singelton

---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Singelton!");
+
+            Logger.Instance.Log("Hallo Logger");
+
+            Logger.Instance.Log("Nochmal Hallo Logger");
+
+        }
+    }
+
+    class Logger
+    {
+        private static Logger instance = null;
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Logger();
+                return instance;
+            }
+        }
+
+        static int instanceCounter = 0;
+        private Logger()
+        {
+            instanceCounter++;
+            Log("Logger started");
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now}] [{instanceCounter}]  {msg}");
+        }
+    }
+}
