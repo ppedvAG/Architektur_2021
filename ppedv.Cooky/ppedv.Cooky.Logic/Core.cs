b6724151971f05efbbac6671dff1df65eec94fb2
André <ruland@outlook.de>
2021-06-01 16:15:16 +0200

    Simple Business Core

---
+++ ppedv.Cooky/ppedv.Cooky.Logic/Core.cs
@@ -0,0 +1,24 @@
+﻿using ppedv.Cooky.Model;
+using ppedv.Cooky.Model.Contracts;
+using System.Linq;
+
+namespace ppedv.Cooky.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public Core() : this(new Data.EFCore.EfCoreRepository())
+        { }
+
+        public int CountRezepteInDb()
+        {
+            return Repository.GetAll<Rezept>().Count();
+        }
+    }
+}
